ffd8a4d98c8746acc3e06e425e34da3b9eb1d4f3
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-10-12 14:13:15 -0400

    commit tree bfde718ee73acb19d0e5b490d344db3cefd758c0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: "Bob", last_name: "Ho", grade: 1)
+Student.create(first_name: "Cindy", last_name: "Ro", grade: 2)
+Student.create(first_name: "Pickle", last_name: "Bo", grade: 3)
+Student.create(first_name: "Dwayne", last_name: "Johnson", grade: 4)
